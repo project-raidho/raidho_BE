bfc2e04498c9266d97e185307aa176b810066c48
rio <sw_park0205@naver.com>
2022-09-05 05:33:31 +0900

    [feat] facebook Oauth2.0 feat

--- src/main/java/com/project/raidho/controller/OAuthLoginController.java
+++ src/main/java/com/project/raidho/controller/OAuthLoginController.java
@@ -1,8 +1,9 @@
 package com.project.raidho.controller;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.project.raidho.service.FacebookMemberService;
 import com.project.raidho.service.KakaoMemberService;
-import com.project.raidho.service.NaverUserService;
+import com.project.raidho.service.NaverMemberService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
@@ -21,8 +22,9 @@ import javax.servlet.http.HttpServletResponse;
 public class OAuthLoginController {
 
     private final KakaoMemberService kakaoMemberService;
+    private final NaverMemberService naverMemberService;
+    private final FacebookMemberService facebookMemberService;
 
-    private final NaverUserService naverUserService;
 
     // 카카오 로그인
     @GetMapping("/kakao")
@@ -37,13 +39,24 @@ public class OAuthLoginController {
     }
 
     @GetMapping("/naver")
-    public ResponseEntity<?> naverLogin(@RequestParam String code, @RequestParam String state, HttpServletResponse response) throws JsonProcessingException {
+    public ResponseEntity<?> naverLogin (@RequestParam String code, @RequestParam String state, HttpServletResponse response) {
         try {
-            naverUserService.naverLogin(code, state, response);
+            naverMemberService.naverLogin(code, state, response);
             return new ResponseEntity<>("네이버 로그인 성공", HttpStatus.OK);
         } catch (JsonProcessingException e) {
             log.error(e.getMessage());
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("카카오 로그인 실패");
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("네이버 로그인 실패");
+        }
+    }
+
+    @GetMapping("/facebook")
+    public ResponseEntity<?> facebookLogin (@RequestParam String code, HttpServletResponse response) {
+        try {
+            facebookMemberService.facebookLogin(code, response);
+            return new ResponseEntity<>("페이스북 로그인 성공", HttpStatus.OK);
+        } catch (JsonProcessingException e) {
+            log.error(e.getMessage());
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("페이스북 로그인 실패");
         }
     }
 }
--- src/main/java/com/project/raidho/service/FacebookMemberService.java
+++ src/main/java/com/project/raidho/service/FacebookMemberService.java
@@ -1,6 +1,5 @@
 package com.project.raidho.service;
 
-
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -19,30 +18,29 @@ import org.springframework.stereotype.Service;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
 import org.springframework.web.client.RestTemplate;
-
 import javax.servlet.http.HttpServletResponse;
 
 @Slf4j
 @RequiredArgsConstructor
 @Service
-public class NaverUserService {
+public class FacebookMemberService {
 
-    @Value("${spring.security.oauth2.client.registration.naver.client-id}")
-    String naverClientId;
+    @Value("${spring.security.oauth2.client.registration.facebook.client-id}")
+    String facebookClientId;
 
-    @Value("${spring.security.oauth2.client.registration.naver.client-secret}")
-    String naverClientSecret;
+    @Value("${spring.security.oauth2.client.registration.facebook.client-secret}")
+    String facebookClientSecret;
 
     private final MemberRepository memberRepository;
 
-    public void naverLogin(String code, String state, HttpServletResponse response) throws JsonProcessingException {
+    public void facebookLogin(String code, HttpServletResponse response) throws JsonProcessingException {
 
-        String NaverResourceToken = getResourceToken(code, state);
-        OauthMemberInfoImpl naverMemberInfo = getNaverMemberInfo(NaverResourceToken);
-        Member naverMember = joinMemberShip(naverMemberInfo);
+        String facebookResourceToken = getResourceToken(code);
+        OauthMemberInfoImpl facebookMemberInfo = getFacebookMemberInfo(facebookResourceToken);
+        Member facebookMember = joinMemberShip(facebookMemberInfo);
     }
 
-    private String getResourceToken(String code, String state) throws JsonProcessingException {
+    private String getResourceToken(String code) throws JsonProcessingException {
         // naver Server 에서 받을 Http 를 만들기 위해 Http Header, body 생성
         // header 생성
         HttpHeaders httpHeaders = new HttpHeaders();
@@ -50,17 +48,17 @@ public class NaverUserService {
         // body
         MultiValueMap<String, String> body = new LinkedMultiValueMap<>();
         body.add("grant_type", "authorization_code");
-        body.add("client_id", naverClientId);
-        body.add("client_secret", naverClientSecret);
+        body.add("client_id", facebookClientId);
+        body.add("client_secret", facebookClientSecret);
+        body.add("redirect_uri", "http://localhost:8080/login/oauth2/code/facebook");
         body.add("code", code);
-        body.add("state", state);
         //POST 요청 보냄
-        HttpEntity<MultiValueMap<String, String>> naverResourceTokenRequest = new HttpEntity<>(body, httpHeaders);
+        HttpEntity<MultiValueMap<String, String>> facebookResourceTokenRequest = new HttpEntity<>(body, httpHeaders);
         RestTemplate restTemplate = new RestTemplate();
         ResponseEntity<String> response = restTemplate.exchange(
-                "https://nid.naver.com/oauth2.0/token",
+                "https://graph.facebook.com/v3.0/oauth/access_token",
                 HttpMethod.POST,
-                naverResourceTokenRequest,
+                facebookResourceTokenRequest,
                 String.class
         );
 
@@ -71,18 +69,18 @@ public class NaverUserService {
         return jsonNode.get("access_token").asText();
     }
 
-    private OauthMemberInfoImpl getNaverMemberInfo(String NaverResourceToken) throws JsonProcessingException{
+    private OauthMemberInfoImpl getFacebookMemberInfo(String FacebookResourceToken) throws JsonProcessingException{
 
         HttpHeaders httpHeaders = new HttpHeaders();
-        httpHeaders.add("Authorization", "Bearer "+ NaverResourceToken);
+        httpHeaders.add("Authorization", "Bearer "+ FacebookResourceToken);
         httpHeaders.add("Content-type", "application/x-www-form-urlencoded;charset=utf-8");
 
-        HttpEntity<MultiValueMap<String, String>> naverResourceInfoRequest = new HttpEntity<>(httpHeaders);
+        HttpEntity<MultiValueMap<String, String>> facebookResourceInfoRequest = new HttpEntity<>(httpHeaders);
         RestTemplate restTemplate = new RestTemplate();
         ResponseEntity<String> response = restTemplate.exchange(
-                "https://openapi.naver.com/v1/nid/me",
+                "https://graph.facebook.com/v3.0/me?fields=id,first_name,middle_name,last_name,name,email,verified",
                 HttpMethod.POST,
-                naverResourceInfoRequest,
+                facebookResourceInfoRequest,
                 String.class
         );
 
@@ -90,24 +88,24 @@ public class NaverUserService {
         ObjectMapper objectMapper = new ObjectMapper();
         JsonNode jsonNode = objectMapper.readTree(ResponseResourceInfo);
 
-        String providerId = jsonNode.get("response").get("id").asText();
-        String memberName = jsonNode.get("response").get("nickname").asText();
-        String email = jsonNode.get("response").get("email").asText();
-        String provider = "naver";
+        String providerId = jsonNode.get("id").asText();
+        String memberName = jsonNode.get("name").asText();
+        String email = jsonNode.get("email").asText();
+        String provider = "facebook";
 
         return new OauthMemberInfoImpl(memberName, email, providerId, provider);
     }
 
-    private Member joinMemberShip(OauthMemberInfoImpl naverMemberInfo) {
-        String provider = naverMemberInfo.getProvider();
-        String providerId = naverMemberInfo.getProviderId();
-        String memberName = naverMemberInfo.getMemberName();
-        Member naverMember = memberRepository.findMember(provider, providerId, memberName)
+    private Member joinMemberShip(OauthMemberInfoImpl facebookMemberInfo) {
+        String provider = facebookMemberInfo.getProvider();
+        String providerId = facebookMemberInfo.getProviderId();
+        String memberName = facebookMemberInfo.getMemberName();
+        Member facebookMember = memberRepository.findMember(provider, providerId, memberName)
                 .orElse(null);
 
         // 회원가입이 되어있지 않으면 Null
-        if (naverMember == null) {
-            String email = naverMemberInfo.getEmail();
+        if (facebookMember == null) {
+            String email = facebookMemberInfo.getEmail();
             String memberImage = null; // Todo :: default image 필요
             String memberIntro = "인사말을 등록해주세요.";
             MemberRole role = MemberRole.USER;
@@ -125,6 +123,7 @@ public class NaverUserService {
             memberRepository.save(member);
             return member;
         }
-        return naverMember;
+        return facebookMember;
     }
 }
+
