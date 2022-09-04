883355835f8785a408a82f28ffbc217752632fec
rio <sw_park0205@naver.com>
2022-09-04 15:53:25 +0900

    [feat] base tool

---
+++ src/main/java/com/project/raidho/RaidhoApplication.java
@@ -0,0 +1,13 @@
+package com.project.raidho;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RaidhoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RaidhoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/project/raidho/RaidhoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.raidho;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RaidhoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
