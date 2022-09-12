d23cfe48ee8f5af04a823adf51509154b0b1328d
Taehun <108983141+JeonTaehun@users.noreply.github.com>
2022-09-12 13:37:05 +0900

    [feat] PostHeart

--- src/main/java/com/project/raidho/repository/PostRepository.java
+++ src/main/java/com/project/raidho/repository/PostRepository.java
@@ -1,11 +1,13 @@
 package com.project.raidho.repository;
 
 import com.project.raidho.domain.Post;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface PostRepository extends JpaRepository<Post,Long> {
-
     List<Post>findAllByOrderByModifiedAtDesc();
+    Optional<Post> findByIdAndActivateIsTrue(Long id);
 }
