f7937fc633be95ad12f47551034e84a5f3a2c88d
rio <sw_park0205@naver.com>
2022-09-16 16:41:14 +0900

    [feat] meeting category 등록 02

--- src/main/java/com/project/raidho/controller/CategoryController.java
+++ src/main/java/com/project/raidho/controller/CategoryController.java
@@ -1,31 +1,19 @@
 package com.project.raidho.controller;
 
+import com.amazonaws.Response;
 import com.project.raidho.domain.ResponseDto;
 import com.project.raidho.domain.meetingPost.dto.ThemeCategoryRequestDto;
 import com.project.raidho.service.CategoryService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
 @RestController
 public class CategoryController {
 
     private final CategoryService categoryService;
-
-    @PostMapping("/api/category")
-    public ResponseDto<?> createCategory(@RequestBody ThemeCategoryRequestDto requestDto,
-                                         @AuthenticationPrincipal UserDetails userDetails) {
-        if (requestDto.getCountryName() == null) {
-            return ResponseDto.fail(404, "카테고리 정보를 입력해주세요.");
-        }
-        categoryService.createCategory(requestDto,userDetails);
-        return null;
-    }
 
     // Todo :: postman test 용
     @PostMapping("/api/category/test")
@@ -37,4 +25,19 @@ public class CategoryController {
         return ResponseDto.success(requestDto.getCountryName());
     }
 
+    @PostMapping("/api/category")
+    public ResponseDto<?> createCategory(@RequestBody ThemeCategoryRequestDto requestDto,
+                                         @AuthenticationPrincipal UserDetails userDetails) {
+        if (requestDto.getCountryName() == null) {
+            return ResponseDto.fail(404, "카테고리 정보를 입력해주세요.");
+        }
+        categoryService.createCategory(requestDto,userDetails);
+        return null;
+    }
+
+    @PutMapping("/api/category/{id}")
+    public ResponseDto<?> updateCategory(@PathVariable Long id,
+                                         @AuthenticationPrincipal UserDetails userDetails) {
+        return null;
+    }
 }
