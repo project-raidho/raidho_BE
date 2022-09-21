1fc7d04142b60a4961b3da09bd8a4e3644405184
Taehun <108983141+JeonTaehun@users.noreply.github.com>
2022-09-21 22:58:29 +0900

    [fix] WebSockConfig registerStompEndpoints setAllowedOrigins

--- src/main/java/com/project/raidho/config/WebSockConfig.java
+++ src/main/java/com/project/raidho/config/WebSockConfig.java
@@ -19,6 +19,7 @@ public class WebSockConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/ws-stomp")
+                .setAllowedOrigins("https://raidho.site")
                 .setAllowedOrigins("http://localhost:3000")
                 .withSockJS();
     }
