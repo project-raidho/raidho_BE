ff1d81bfd6b74124f1fb9722f08af4f8d6d31849
rio <sw_park0205@naver.com>
2022-09-24 00:09:19 +0900

    [feat] 채팅방 탈퇴하기 / exit

--- src/main/java/com/project/raidho/repository/RoomDetailRepository.java
+++ src/main/java/com/project/raidho/repository/RoomDetailRepository.java
@@ -16,10 +16,10 @@ public interface RoomDetailRepository extends JpaRepository<RoomDetail, Long> {
 
     void deleteByRoomMaster_RoomId(Long roomId);
 
+    void deleteByRoomMasterAndMember(RoomMaster roomMaster, Member member);
+
     @Query("SELECT COUNT(d) FROM RoomDetail d WHERE d.roomMaster = :roomMaster")
     int getCountJoinRoomMember(@Param(value = "roomMaster") RoomMaster roomMaster);
-
-    List<Long> findAllByRoomMaster(RoomMaster roomMaster);
 
     @Query("SELECT d.member.id FROM RoomDetail d WHERE d.roomMaster = :roomMaster")
     List<Long> getAllMemberId(@Param(value = "roomMaster") RoomMaster roomMaster);
