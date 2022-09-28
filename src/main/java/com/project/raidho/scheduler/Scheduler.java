3b1983176b1658b73521a9acb1211f7e83c8d976
rio <sw_park0205@naver.com>
2022-09-29 02:49:13 +0900

    [feat] 모집글에 작성된 여행 endDate 다음날 모집글 자동 삭제 Scheduler 추가

---
+++ src/main/java/com/project/raidho/scheduler/Scheduler.java
@@ -0,0 +1,42 @@
+package com.project.raidho.scheduler;
+
+import com.project.raidho.domain.meetingPost.MeetingPost;
+import com.project.raidho.repository.ChatMessageRepository;
+import com.project.raidho.repository.MeetingPostRepository;
+import com.project.raidho.repository.RoomDetailRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+
+@Slf4j
+@RequiredArgsConstructor
+@Component
+public class Scheduler {
+
+    private final MeetingPostRepository meetingPostRepository;
+    private final ChatMessageRepository chatMessageRepository;
+    private final RoomDetailRepository roomDetailRepository;
+
+    @Scheduled(cron = "* 1 * * * *") // 초(0-59) 분(0-59) 시(0-23) 일(1-31) 월(1-12) 요일(1-7)
+    @Transactional
+    public void deleteMeetingPost() throws ParseException {
+        SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
+        List<MeetingPost> meetingPosts = meetingPostRepository.findAll();
+        for (MeetingPost meetingPost : meetingPosts) {
+            Date date = formatter.parse(meetingPost.getEndDate());
+            Date tomorrow = new Date(date.getTime() + (1000 * 60 * 60 * 24));
+            if (tomorrow.before(new Date())) {
+                chatMessageRepository.deleteAllByRoomId(meetingPost.getId());
+                roomDetailRepository.deleteByRoomMaster_RoomId(meetingPost.getId());
+                meetingPostRepository.delete(meetingPost);
+            }
+        }
+    }
+}
