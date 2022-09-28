103676e8e3b98871ab1c6adb4e7b934297034b04
rio <sw_park0205@naver.com>
2022-09-29 00:05:16 +0900

    [fix] 채팅방입장시 어떤 회원이 접속했는지, test 07

--- src/main/java/com/project/raidho/controller/ChatMessageController.java
+++ src/main/java/com/project/raidho/controller/ChatMessageController.java
@@ -2,7 +2,10 @@ package com.project.raidho.controller;
 
 import com.project.raidho.domain.chat.ChatDto.ChatMessageDto;
 import com.project.raidho.domain.chat.ChatMessage;
+import com.project.raidho.domain.member.Member;
+import com.project.raidho.jwt.JwtTokenProvider;
 import com.project.raidho.redis.RedisPublisher;
+import com.project.raidho.repository.ChatMessageRepository;
 import com.project.raidho.service.ChatMessageService;
 import com.project.raidho.service.RoomService;
 import lombok.RequiredArgsConstructor;
@@ -11,8 +14,10 @@ import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.Header;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.simp.SimpMessageSendingOperations;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.Map;
 
 @RequiredArgsConstructor
@@ -24,18 +29,17 @@ public class ChatMessageController {
     private final ChatMessageService chatMessageService;
     private final RedisPublisher redisPublisher;
     private final RoomService roomService;
+    private final ChatMessageRepository chatMessageRepository;
 
     @MessageMapping("/chat/send/{roomId}")
     public void getRoomChats(@DestinationVariable Long roomId, ChatMessageDto chatMessageDto, @Header("token") String token) {
 
-        System.out.println("================================================");
-        System.out.println(token);
-        System.out.println("================================================");
-
         if (ChatMessage.Type.ENTER.equals(chatMessageDto.getType())) {
-            chatMessageDto.setMessage(chatMessageDto.getSender() + "님이 입장하셨습니다.");
+            chatMessageDto.setMessage(chatMessageDto.getSender() + " 님이 입장하셨습니다.");
+            ChatMessage chatMessage = ChatMessage.builder().message(chatMessageDto.getMessage())
+                            .type(chatMessageDto.getType()).sender(chatMessageDto.getSender()).roomId(roomId).build();
+            chatMessageRepository.save(chatMessage);
             roomService.enterChatRoom(String.valueOf(roomId));
-            ChatMessageDto returnChatMessageDto = chatMessageService.saveChatMessage(roomId, chatMessageDto); // db 메시지 저장
             redisPublisher.publish(roomService.getTopic(String.valueOf(roomId)),chatMessageDto);
         } else {
             roomService.enterChatRoom(String.valueOf(roomId));
