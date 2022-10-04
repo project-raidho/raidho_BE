6b2a19e2b5797e19c3e93b1b1004be2af867c404
rio <sw_park0205@naver.com>
2022-10-05 05:07:27 +0900

    [fix] @Lock(LockModeType.PESSIMISTIC_WRITE)

--- src/main/java/com/project/raidho/repository/RoomDetailRepository.java
+++ src/main/java/com/project/raidho/repository/RoomDetailRepository.java
@@ -4,9 +4,11 @@ import com.project.raidho.domain.chat.RoomDetail;
 import com.project.raidho.domain.chat.RoomMaster;
 import com.project.raidho.domain.member.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import javax.persistence.LockModeType;
 import java.util.List;
 import java.util.Optional;
 
@@ -20,6 +22,7 @@ public interface RoomDetailRepository extends JpaRepository<RoomDetail, Long> {
 
     void deleteByRoomMasterAndMember(RoomMaster roomMaster, Member member);
 
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
     @Query("SELECT COUNT(d) FROM RoomDetail d WHERE d.roomMaster = :roomMaster")
     int getCountJoinRoomMember(@Param(value = "roomMaster") RoomMaster roomMaster);
 
